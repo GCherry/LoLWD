1c0fdd67be3457ce16df157373afdc3129ea8021
gcherry <gcherry2@gmail.com>
2015-10-17 16:12:48 -0400

    Initial commit

---
+++ LoLWD.web/LoLWD.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoLWD.web.MvcApplication" Language="C#" %>
---
+++ LoLWD.web/LoLWD.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoLWD.web.Startup))]
+namespace LoLWD.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
