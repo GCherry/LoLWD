ba29b1e0677918a19658230656930e7823d031b0
gcherry <gcherry2@gmail.com>
2015-10-17 16:41:49 -0400

    Clean up

---
+++ LoLWD.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoLWD.Startup))]
+namespace LoLWD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
