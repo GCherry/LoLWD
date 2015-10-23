aad9790f3af06b99ef055b068b2db0f267a6fd94
gcherry <gcherry2@gmail.com>
2015-10-22 22:44:44 -0400

    New project files and general setup

---
+++ LoLWD.Data/Context/LoLDBContext.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace LoLWD.Data.Context
+{
+    public class LoLDBContext : DbContext
+    {
+        #region Properties
+
+        public static string ConnectionStringName { get; set; }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //modelBuilder.Configurations.Add(new ChampionModelMap());
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public LoLDBContext() : base(ConnectionStringName)
+        {
+        }
+
+        static LoLDBContext()
+        {
+            ConnectionStringName = "LoLConnection";
+        }
+
+        #endregion
+    }
+}
