3d3fb9a933f72500e2096f00f7ba1ce007cd90d9
johncher <jcherry022@gmail.com>
2015-11-01 17:03:46 -0500

    Added contact table -
    added contact table to eventually display contact info on web page.

---
+++ LoLWD.Data/ModelMaps/ContactModelMap.cs
@@ -0,0 +1,21 @@
+﻿using LoLWD.Shared.Models.Entities;
+
+namespace LoLWD.Data.ModelMaps
+{
+    public class ContactModelMap : BaseEnityModelMap<Contact>
+    {
+        #region Constructors
+
+        public ContactModelMap()
+        {
+            Property(x => x.Name).IsRequired().HasMaxLength(200);
+            Property(x => x.Email).IsRequired().HasMaxLength(200);
+            Property(x => x.Comment).IsOptional().HasMaxLength(700);
+
+            ToTable("Contact");
+        }
+
+        #endregion
+
+    }
+}
---
+++ LoLWD.Shared/Models/Entities/Contact.cs
@@ -0,0 +1,15 @@
+﻿namespace LoLWD.Shared.Models.Entities
+
+{
+    public class Contact : Entity
+    {
+        #region Properties
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+
+        #endregion
+
+    }
+}
